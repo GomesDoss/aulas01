a1d6f7004c4ab475a1542badd93f6bc72e2cceef
Charles Gomes <charles.gomes3721@gmail.com>
2024-06-13 09:32:04 -0400

    charles.gomes3721@gmail.com

---
+++ aula01.py
@@ -0,0 +1,17 @@
+x=1 #int
+x='CFB Cursos' #string
+x=15.6 #float
+x=True #bool
+n1=5; n2=2;  x=complex(n1,n2)
+x=['Carro', 'Avião' , 'Navio',1,58.3,True] #List / Array
+x=('Carro', 'Avião' , 'Navio',1,58.3,True) #tupla
+x=range(0,100,1) #List
+x={
+    'canal':'CFB Cursos',
+    'curso':'Curso de Python',
+    'nome':'Bruno'
+}
+x={5,7,4,5,7,4,8} #set
+x=frozenset({5,7,4,5,7,4,8}) #set
+print('Valor: '+str(x))
+print('tipo:'+str(type(x)))
